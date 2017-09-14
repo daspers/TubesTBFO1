dd2d39c0500dc03e2199607e519a33814562becc
Tony <buddy90_lost@yahoo.co.id>
2017-09-14 23:55:35 +0700

    Update dfa.h

--- dfa.h
+++ dfa.h
@@ -1,1 +1,21 @@
+#ifndef DFA_H
+#define DFA_H
 
+#include "boolean.h"
+
+typedef struct{
+	char state[10];
+	int statenum;
+	boolean finalstate;
+} STATUS;
+
+typedef struct{
+	STATUS S[100];
+	int Neff;
+} ArrSTATUS;
+
+typedef struct{
+	
+} RELASI;
+
+#endif
