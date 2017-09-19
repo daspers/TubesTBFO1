22a535bcd52970fe05c29aece859bce560d266ca
Tony <buddy90_lost@yahoo.co.id>
2017-09-19 19:20:10 +0700

    Update dfa.h

--- dfa.h
+++ dfa.h
@@ -5,17 +5,35 @@
 
 typedef struct{
 	char state[10];
-	int statenum;
 	boolean finalstate;
 } STATUS;
 
 typedef struct{
-	STATUS S[100];
+	STATUS S[50];
 	int Neff;
 } ArrSTATUS;
 
 typedef struct{
-	
+	char alpha[10];
+} ALPHABET;
+
+typedef struct{
+	ALPHABET A[50];
+	int Neff;
+} ArrALPHA;
+
+typedef struct{
+	STATUS inState[1000];
+	ALPHABET Alphabet[1000];
+	STATUS fState[1000];
+	int Neff;
 } RELASI;
+
+void MakeEmptyArrStatus(ArrSTATUS *TabS);
+
+void MakeEmptyArrALPHA(ArrALPHA *TabA);
+
+void BacaFile(ArrSTATUS *TabS, ArrALPHA *TabA);
+
 
 #endif
