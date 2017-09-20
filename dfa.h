99a4a6f5a29f5f422086ed231016651662093f86
Tony <buddy90_lost@yahoo.co.id>
2017-09-20 21:46:12 +0700

    Update dfa.h

--- dfa.h
+++ dfa.h
@@ -3,13 +3,15 @@
 
 #include "boolean.h"
 
+#define IdxUndef -1
+
 typedef struct{
 	char state[10];
 	boolean finalstate;
 } STATUS;
 
 typedef struct{
-	STATUS S[50];
+	STATUS S[35];
 	int Neff;
 } ArrSTATUS;
 
@@ -18,14 +20,14 @@ typedef struct{
 } ALPHABET;
 
 typedef struct{
-	ALPHABET A[50];
+	ALPHABET A[35];
 	int Neff;
 } ArrALPHA;
 
 typedef struct{
-	STATUS inState[1000];
-	ALPHABET Alphabet[1000];
-	STATUS fState[1000];
+	STATUS inState[1225];
+	ALPHABET Alphabet[1225];
+	STATUS fState[1225];
 	int Neff;
 } RELASI;
 
@@ -33,7 +35,12 @@ void MakeEmptyArrStatus(ArrSTATUS *TabS);
 
 void MakeEmptyArrALPHA(ArrALPHA *TabA);
 
-void BacaFile(ArrSTATUS *TabS, ArrALPHA *TabA);
+void BacaFile(ArrSTATUS *TabS, ArrALPHA *TabA, RELASI *R, STATUS *start);
 
+void TulisData(ArrSTATUS TabS, ArrALPHA TabA, RELASI R, STATUS start);
+
+int CariState(ArrSTATUS TabS, char N[]);
+
+boolean CompStatus (char A[], char B[]);
 
 #endif
