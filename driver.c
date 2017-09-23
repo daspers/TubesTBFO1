369a4af80be0625a2561156187804eab57791e32
Tony <buddy90_lost@yahoo.co.id>
2017-09-24 01:05:44 +0700

    Update driver.c

--- driver.c
+++ driver.c
@@ -2,11 +2,16 @@
 #include "boolean.h"
 
 int main(){
+	//Kamus
 	ArrSTATUS TabS;
 	ArrALPHA TabA;
+	ArrINPUT TabI;
 	RELASI R;
-	STATUS start;
+	int start;
+	//Algoritma
 	BacaFile(&TabS, &TabA, &R, &start);
 	TulisData(TabS, TabA, R, start);
+	BacaInputDFA(TabA, R, &TabI, start);
+	TulisHasil(TabS,TabA,TabI,R,start);
 	return 0;
 }
