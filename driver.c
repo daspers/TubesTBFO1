a19e7fe61bc7fb795e90b359c4ea2d9f6eebfe32
Tony <buddy90_lost@yahoo.co.id>
2017-09-25 20:51:29 +0700

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,17 +1,20 @@
 #include "dfa.h"
-#include "boolean.h"
 
 int main(){
-	//Kamus
+	/* Kamus */
 	ArrSTATUS TabS;
 	ArrALPHA TabA;
 	ArrINPUT TabI;
 	RELASI R;
-	int start;
-	//Algoritma
-	BacaFile(&TabS, &TabA, &R, &start);
-	TulisData(TabS, TabA, R, start);
-	BacaInputDFA(TabA, R, &TabI, start);
-	TulisHasil(TabS,TabA,TabI,R,start);
+	int IdxStart;
+	/* Algoritma */
+	//Baca DFA dari file external dan disimpan ke variabel
+	BacaFile(&TabS, &TabA, &R, &IdxStart);
+	//Tulis data-data DFA yang dibaca ke layar console/terminal
+	TulisData(TabS, TabA, R, IdxStart);
+	//Baca Input DFA dari file external dan disimpan ke variabel
+	BacaInputDFA(TabA, R, &TabI, IdxStart);
+	//Tulis hasil dari Input DFA ke file external
+	TulisHasil(TabS, TabA, TabI, R, IdxStart);
 	return 0;
 }
